e25c94b1e75a0806abb20df91b826d5c127aac41
Josephine Cattell <josieohio7967@gmail.com>
2026-04-19 23:12:13 -0400

    refractor fleet

--- alien_fleet.py
+++ alien_fleet.py
@@ -31,26 +31,35 @@ class AlienFleet:
         alien_h = self.settings.alien_h
         screen_h = self.settings.screen_h
         
+        fleet_w, fleet_h, x_offset, y_offset = self.calculate_offsets(alien_w, screen_w, alien_h, screen_h)
+       
+       # if(level == 1):
+        self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
+
+    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
+        for row in range(fleet_h):
+            for col in range(fleet_w):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
+                if col % 2 == 0 or row % 2 == 0:
+                    continue
+                self._create_alien(current_x, current_y)
+
+    def calculate_offsets(self, alien_w, screen_w, alien_h, screen_h):
         half_screen = self.settings.screen_h/2
         fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
         
         fleet_horizontal_space = fleet_w * alien_w
         fleet_vertical_space = fleet_h * alien_h
         x_offset = int((screen_w - fleet_horizontal_space)//2)
-        y_offset = int((half_screen -fleet_vertical_space)//2)
-        
-        for row in range(fleet_h):
-            for col in range(fleet_w):
-                current_x = alien_w * col + x_offset
-                current_y = alien_h * row + y_offset
-                if col % 2 == 0 or row + y_offset:
-                    continue
-                self._create_alien(current_x, current_y)
+        y_offset = int((half_screen - fleet_vertical_space)//2)
+        return fleet_w,fleet_h,x_offset,y_offset
+           
 
 
     def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
         fleet_w = (screen_w // alien_w)
-        fleet_h = (screen_h/2// alien_h)
+        fleet_h = (screen_h/2//alien_h)
         if fleet_w % 2 == 0:
             fleet_w -= 1
         else:
@@ -59,7 +68,10 @@ class AlienFleet:
             fleet_h -= 1
         else:
             fleet_h -= 2
+        
+
         return int(fleet_w), int(fleet_h)
+    
             
     def _create_alien(self, current_x, current_y):
         new_alien = Alien(self, current_x, current_y)
